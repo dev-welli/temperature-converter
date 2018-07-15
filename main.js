cf61930c35222d84c7ff07ad7239f86520c0ac1a
dev-welli <welli.abdullah@xero.com>
2018-07-15 16:33:17 +1200

    refactored

--- main.js
+++ main.js
@@ -5,28 +5,34 @@ const kelvinInput = document.querySelector('#kelvin > input');//getting the inpu
 
 //console.log(kelvinInput);
 
-//listening to input event in celcius div
-celciusInput.addEventListener('input', function(){
+function celsiusToFahrenheitAndKelvin(){
    const celciusTemperature = parseFloat(celciusInput.value);
    const fahrenheitTemperature = (celciusTemperature * (9/5)) + 32;
    const kelvinTemperature = celciusTemperature + 273.15;
    fahrenheitInput.value = fahrenheitTemperature;
    kelvinInput.value = kelvinTemperature;
    console.log(kelvinTemperature);
-});
+}
 
-fahrenheitInput.addEventListener('input', function(){
+function fahrenheitToCelciusAndKelvin(){
    const fahrenheitTemperature = parseFloat(fahrenheitInput.value);
    const celciusTemperature = (fahrenheitTemperature - 32) * (5/9);
    const kelvinTemperature =  (fahrenheitTemperature + 459.67) * (5/9);
    celciusInput.value = celciusTemperature;
    kelvinInput.value = kelvinTemperature;
-});
+}
 
-kelvinInput.addEventListener('input', function(){
+function kelvinToCelciusAndFahrenheit(){
    const kelvinTemperature = parseFloat(kelvinInput.value);
    const celciusTemperature = kelvinTemperature - 273.15;
    const fahrenheitTemperature = 1.8 * (kelvinTemperature - 273) + 32;
    celciusInput.value = celciusTemperature;
    fahrenheitInput.value = fahrenheitTemperature;
-});
+}
+
+//listening to input events in divs
+celciusInput.addEventListener('input', celsiusToFahrenheitAndKelvin);
+
+fahrenheitInput.addEventListener('input', fahrenheitToCelciusAndKelvin);
+
+kelvinInput.addEventListener('input', kelvinToCelciusAndFahrenheit);
