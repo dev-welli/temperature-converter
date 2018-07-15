47996f2b615294195537533ce4649564f3b8e57b
dev-welli <welli.abdullah@xero.com>
2018-07-15 16:45:19 +1200

    add rounding and main functions

--- main.js
+++ main.js
@@ -1,38 +1,42 @@
+const celciusInput = document.querySelector('#celcius > input'); //getting the input inside the celcius div
+const fahrenheitInput = document.querySelector('#fahrenheit > input'); //getting the input inside the fahrenheit div
+const kelvinInput = document.querySelector('#kelvin > input'); //getting the input inside the kelvin div
 
-const celciusInput = document.querySelector('#celcius > input');//getting the input inside the celcius div
-const fahrenheitInput = document.querySelector('#fahrenheit > input');//getting the input inside the fahrenheit div
-const kelvinInput = document.querySelector('#kelvin > input');//getting the input inside the kelvin div
 
-//console.log(kelvinInput);
-
-function celsiusToFahrenheitAndKelvin(){
+function celsiusToFahrenheitAndKelvin() {
    const celciusTemperature = parseFloat(celciusInput.value);
-   const fahrenheitTemperature = (celciusTemperature * (9/5)) + 32;
+   const fahrenheitTemperature = (celciusTemperature * (9 / 5)) + 32;
    const kelvinTemperature = celciusTemperature + 273.15;
-   fahrenheitInput.value = fahrenheitTemperature;
-   kelvinInput.value = kelvinTemperature;
-   console.log(kelvinTemperature);
+   fahrenheitInput.value = roundTwoDigits(fahrenheitTemperature);
+   kelvinInput.value = roundTwoDigits(kelvinTemperature);
 }
 
-function fahrenheitToCelciusAndKelvin(){
+function fahrenheitToCelciusAndKelvin() {
    const fahrenheitTemperature = parseFloat(fahrenheitInput.value);
-   const celciusTemperature = (fahrenheitTemperature - 32) * (5/9);
-   const kelvinTemperature =  (fahrenheitTemperature + 459.67) * (5/9);
-   celciusInput.value = celciusTemperature;
-   kelvinInput.value = kelvinTemperature;
+   const celciusTemperature = (fahrenheitTemperature - 32) * (5 / 9);
+   const kelvinTemperature = (fahrenheitTemperature + 459.67) * (5 / 9);
+   celciusInput.value = roundTwoDigits(celciusTemperature);
+   kelvinInput.value = roundTwoDigits(kelvinTemperature);
 }
 
-function kelvinToCelciusAndFahrenheit(){
+function kelvinToCelciusAndFahrenheit() {
    const kelvinTemperature = parseFloat(kelvinInput.value);
    const celciusTemperature = kelvinTemperature - 273.15;
    const fahrenheitTemperature = 1.8 * (kelvinTemperature - 273) + 32;
-   celciusInput.value = celciusTemperature;
-   fahrenheitInput.value = fahrenheitTemperature;
+   celciusInput.value = roundTwoDigits(celciusTemperature);
+   fahrenheitInput.value = roundTwoDigits(fahrenheitTemperature);
 }
 
-//listening to input events in divs
-celciusInput.addEventListener('input', celsiusToFahrenheitAndKelvin);
+function roundTwoDigits(num) {
+   return Math.round(num * 100) / 100;
+}
 
-fahrenheitInput.addEventListener('input', fahrenheitToCelciusAndKelvin);
 
-kelvinInput.addEventListener('input', kelvinToCelciusAndFahrenheit);
+function main() {
+   //listening to input events in divs
+   celciusInput.addEventListener('input', celsiusToFahrenheitAndKelvin);
+   fahrenheitInput.addEventListener('input', fahrenheitToCelciusAndKelvin);
+   kelvinInput.addEventListener('input', kelvinToCelciusAndFahrenheit);
+}
+
+main();
